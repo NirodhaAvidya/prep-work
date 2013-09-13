4cc00eca007424a36af8cf4731954688b4e514b5
NirodhaAvidya <nirodha.avidya@gmail.com>
2013-09-12 23:46:39 -0400

    morse challenge done

--- coding-test-2/practice-problems/lib/05_morse_code.rb
+++ coding-test-2/practice-problems/lib/05_morse_code.rb
@@ -1,2 +1,41 @@
-def morse_encode(str)
+@@morse = {
+"a" => ".-",
+"b" => "-...",
+"c" => "-.-.",
+"d" => "-..",
+"e" => ".",
+"f" => "..-.",
+"g" => "--.",
+"h" => "....",
+"i" => "..",
+"j" => ".---",
+"k" => "-.-",
+"l" => ".-..",
+"m" => "--",
+"n" => "-.",
+"o" => "---",
+"p" => ".--.",
+"q" => "--.-",
+"r" => ".-.",
+"s" => "...",
+"t" => "-",
+"u" => "..-",
+"v" => "...-",
+"w" => ".--",
+"x" => "-..-",
+"y" => "-.--",
+"z" => "--.."}
+
+def morse_encode(str) # "cat in hat"
+  out = []
+  words = str.split(" ") # ["cat","in","hat"]
+  words.each{ |word| out << morse_encode_word(word)}
+  out.join("  ")
+end
+
+def morse_encode_word(word)
+  out = []
+  letters = word.split(//)
+  letters.each{ |l| out << @@morse[l] }
+  out.join(" ")
 end
