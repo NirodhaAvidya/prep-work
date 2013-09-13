ee95477fa7d63294790026402d0bf674fc488977
NirodhaAvidya <nirodha.avidya@gmail.com>
2013-09-13 00:10:48 -0400

    descramble challenge complete

--- coding-test-2/practice-problems/lib/06_word_unscrambler.rb
+++ coding-test-2/practice-problems/lib/06_word_unscrambler.rb
@@ -1,2 +1,7 @@
+   # word_unscrambler("turn", ["numb", "turn", "runt", "nurt"])
+   #    .should == ["turn", "runt", "nurt"]
+
 def word_unscrambler(str, words)
+  sort = str.split(//).sort.join
+  words.select{|word| sort == word.split(//).sort.join }
 end
